9c68b7b34f54f399dee1bca0fef08d3e087044b9
xieyp <xieyp@asiainfo.com>
2020-02-18 22:08:02 +0800

    学习Lucene

---
+++ src/main/java/com/xie/lucene/LuceneTest/LuceneFirst.java
@@ -0,0 +1,170 @@
+package com.xie.lucene.LuceneTest;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.junit.Test;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+public class LuceneFirst {
+	@Test
+    public void createIndex() throws Exception {
+		//1、创建一个Director对象，指定索引库保存的位置。
+        //把索引库保存在内存中
+        //Directory directory = new RAMDirectory();
+        //把索引库保存在磁盘
+		Directory directory = FSDirectory.open(new File("H:\\eclipse\\Lucene\\path").toPath());
+		//2、基于Directory对象创建一个IndexWriter对象
+		IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig());
+		//3、读取磁盘上的文件，对应每个文件创建一个文档对象。
+		File dir = new File("H:\\eclipse\\Lucene\\searchsource");
+		File[]  files = dir.listFiles();
+		for(File file : files){
+			//取文件名
+			String fileName = file.getName();
+			//文件的路径
+			String filePath = file.getPath();
+			//文件的内容
+			String fileContent = FileUtils.readFileToString(file, "utf-8");
+			//文件的大小
+			long fileSize = FileUtils.sizeOf(file);
+			//创建Field
+            //参数1：域的名称，参数2：域的内容，参数3：是否存储
+			Field fieldName = new TextField("name", fileName , Field.Store.YES);
+			//Field fieldPath = new TextField("path", filePath , Field.Store.YES);
+			//因为作为文件的路径放到document对象的域中，但是一般不会用于查询和构建索引，只是用于保存，所以使用StoredField
+			Field fieldPath = new StoredField("path", filePath);
+			Field fieldContent = new TextField("content", fileContent , Field.Store.YES);
+			//Field fieldSize = new TextField("size", fileSize+"" , Field.Store.YES);
+			//因为文件大小是long类型的，所以这里可以使用LongPoint作为存储的域
+			Field fieldSizeValue = new LongPoint("size", fileSize);
+            Field fieldSizeStore = new StoredField("size", fileSize);
+			//创建文档对象
+			Document document = new Document();
+			//向文档对象中添加域
+			document.add(fieldName);
+            document.add(fieldPath);
+            document.add(fieldContent);
+//            document.add(fieldSize);
+            document.add(fieldSizeValue);
+            document.add(fieldSizeStore);
+            //5、把文档对象写入索引库
+            indexWriter.addDocument(document);
+		}
+		//6、关闭indexwriter对象
+        indexWriter.close();
+	}
+	
+	@Test
+	public void searchIndex() throws Exception {
+		//1、创建一个Director对象，指定索引库的位置
+		Directory directory = FSDirectory.open(new File("H:\\eclipse\\Lucene\\path").toPath());
+		//2、创建一个IndexReader对象
+		IndexReader indexReader = DirectoryReader.open(directory);
+		//3、创建一个IndexSearcher对象，构造方法中的参数indexReader对象。
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+		//4、创建一个Query对象，TermQuery
+		Query query = new TermQuery(new Term("content","spring"));
+		//5、执行查询，得到一个TopDocs对象
+        //参数1：查询对象 参数2：查询结果返回的最大记录数
+		TopDocs topDocs = indexSearcher.search(query, 10);
+		//6、取查询结果的总记录数
+        System.out.println("查询总记录数：" + topDocs.totalHits);
+        //7、取文档列表
+        ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+        //8、打印文档中的内容
+        for(ScoreDoc doc :scoreDocs){
+        	//取文档id
+            int docId = doc.doc;
+            //根据id取文档对象
+            Document document = indexSearcher.doc(docId);
+            System.out.println(document.get("name"));
+            System.out.println(document.get("path"));
+            System.out.println(document.get("size"));
+            //System.out.println(document.get("content"));
+            System.out.println("-----------------寂寞的分割线");
+        }
+        //9、关闭IndexReader对象
+        indexReader.close();
+	}
+	
+	@Test
+    public void createIKIndex() throws Exception {
+		//1、创建一个Director对象，指定索引库保存的位置。
+        //把索引库保存在内存中
+        //Directory directory = new RAMDirectory();
+        //把索引库保存在磁盘
+		Directory directory = FSDirectory.open(new File("H:\\eclipse\\Lucene\\path").toPath());
+		//2、基于Directory对象创建一个IndexWriter对象
+		IndexWriterConfig config = new IndexWriterConfig(new IKAnalyzer());
+		IndexWriter indexWriter = new IndexWriter(directory,config);
+		//3、读取磁盘上的文件，对应每个文件创建一个文档对象。
+		File dir = new File("H:\\eclipse\\Lucene\\searchsource");
+		File[]  files = dir.listFiles();
+		for(File file : files){
+			//取文件名
+			String fileName = file.getName();
+			//文件的路径
+			String filePath = file.getPath();
+			//文件的内容
+			String fileContent = FileUtils.readFileToString(file, "utf-8");
+			//文件的大小
+			long fileSize = FileUtils.sizeOf(file);
+			//创建Field
+            //参数1：域的名称，参数2：域的内容，参数3：是否存储
+			Field fieldName = new TextField("name", fileName , Field.Store.YES);
+			Field fieldPath = new TextField("path", filePath , Field.Store.YES);
+			Field fieldContent = new TextField("content", fileContent , Field.Store.YES);
+			Field fieldSize = new TextField("size", fileSize+"" , Field.Store.YES);
+			//创建文档对象
+			Document document = new Document();
+			//向文档对象中添加域
+			document.add(fieldName);
+            document.add(fieldPath);
+            document.add(fieldContent);
+            document.add(fieldSize);
+            //5、把文档对象写入索引库
+            indexWriter.addDocument(document);
+		}
+		//6、关闭indexwriter对象
+        indexWriter.close();
+	}
+	
+	@Test
+    public void testTokenStream() throws Exception {
+		//1）创建一个Analyzer对象，StandardAnalyzer对象
+		Analyzer analyzer = new IKAnalyzer();
+		//2）使用分析器对象的tokenStream方法获得一个TokenStream对象
+		TokenStream tokenStream = analyzer.tokenStream("", "2017年12月14日 - 传智播客Lucene概述公安局Lucene是一款高性能的、可扩展的信息检索(IR)工具库。信息检索是指文档搜索、文档内信息搜索或者文档相关的元数据搜索等操作。");
+		//3）向TokenStream对象中设置一个引用，相当于数一个指针
+		CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+		//4）调用TokenStream对象的rest方法。如果不调用抛异常,rest相当于指针初始化，将指针初始化到栈顶
+		tokenStream.reset();
+		//5）使用while循环遍历TokenStream对象
+		while(tokenStream.incrementToken()){
+			System.out.println(charTermAttribute.toString());
+		}
+		//6）关闭TokenStream对象
+        tokenStream.close();
+	}
+}
